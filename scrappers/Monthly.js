8ca4d899ec5c90daa0ea773552c7b85105a46ddc
mohammad javad bayati <m.javad.bayati97@gmail.com>
2022-02-06 19:18:14 +0330

    Add daily and Monthly scrappers

---
+++ scrappers/Monthly.js
@@ -0,0 +1,57 @@
+cheerio = require('cheerio');
+const crawler = require('../util/selenium');
+
+const utils = {
+    trimText(strInput) {
+        return strInput.replace(/\n/g,'').replace(/\t/g,'');
+    }
+}
+
+class Monthly {
+    constructor(url) {
+        this.url = url;
+    }
+
+    async crawl() {
+        this.dom = cheerio.load(await crawler(this.url));
+    }
+
+    extractHeaders() {
+        const monthlyHeaders = this.dom('.monthly-header .day-text span');
+
+        const headers = [];
+
+        monthlyHeaders.each((idx, element) => {
+            cheerio.load(element).html();
+            headers.push(utils.trimText(this.dom(element).text()));
+        })
+        return headers;
+    }
+
+    extractDayInfo(element) {
+        const dayNumber = utils.trimText(this.dom(element).children('.monthly-panel-top').children('.date').text());
+        const historyAverage = {
+            high: utils.trimText(this.dom(element).children('.history-avg').children('.high').text()),
+            low: utils.trimText(this.dom(element).children('.history-avg').children('.low').text()),
+        }
+        return {dayNumber, historyAverage};
+    }
+
+    extractDailyWhetherInfo() {
+        const daysWhetherList = this.dom('.monthly-daypanel');
+        const data = [];
+        daysWhetherList.each((idx, element) => {
+            data.push(this.extractDayInfo(element));
+        });
+        return data;
+    }
+
+    async extractInfo() {
+        await this.crawl();
+        const data = this.extractDailyWhetherInfo();
+        const headers = this.extractHeaders();
+        console.log(data, headers);
+    }
+}
+
+module.exports = Monthly;
